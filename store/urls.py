a08ade9b608a4a163afda4609088b4b55e7c4ea9
Elimar Afonso <elimar_afonso@hotmail.com>
2023-01-30 13:29:50 -0300

    Implementado Busca/Paginação

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import StoreView, ProductDetailView
+from .views import StoreView, ProductDetailView, SearchListView
 
 
 urlpatterns = [
     path('', StoreView.as_view(), name='store'),
-    path('<slug:category_slug>/', StoreView.as_view(), name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', ProductDetailView.as_view(), name='ProductDetail'),
+    path('category/<slug:category_slug>/', StoreView.as_view(), name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', ProductDetailView.as_view(), name='ProductDetail'),
+    path('search/', SearchListView.as_view(), name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,32 +1,44 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 import carts.views
 from category.models import Category
-from carts.models import CartItem, Cart
+from carts.models import CartItem
 from .models import Product
 
 
-# Create your views here.
-
-
 class StoreView(TemplateView):
-
     template_name = 'store/store.html'
 
-    def get_context_data(self,  **kwargs):
+    def get_context_data(self, **kwargs):
         context = super(StoreView, self).get_context_data(**kwargs)
 
         category_slug = kwargs.get('category_slug')
         # se Category_slug não for NONE é porque tem um slug
-        if  category_slug != None:
+        if category_slug != None:
             categories = get_object_or_404(Category, slug=category_slug)
-            context['products'] = Product.objects.all().filter(category=categories, is_available=True)
-            context['quant_products'] = context['products'].count()
+
+            products_by_category = Product.objects.all().filter(category=categories, is_available=True).order_by('id')
+            # paginando produtos por categorias
+            paginator = Paginator(products_by_category, 1)
+            page = self.request.GET.get('page')
+            paged_products = paginator.get_page(page)
+            context['products'] = paged_products
+            #
+            context['quant_products'] = products_by_category.count()
         else:
             context['categories'] = Category.objects.all()
-            context['products'] = Product.objects.all().filter(is_available=True)
-            context['quant_products'] = context['products'].count()
+
+            # paginando produtos
+            products_all = Product.objects.all().filter(is_available=True).order_by('id')
+            paginator = Paginator(products_all, 5)
+            page = self.request.GET.get('page')
+            paged_products = paginator.get_page(page)
+            context['products'] = paged_products
+
+            context['quant_products'] = products_all.count()
 
         return context
 
@@ -40,7 +52,8 @@ class ProductDetailView(TemplateView):
         product_slug = kwargs.get('product_slug')
         try:
             single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-            in_cart = CartItem.objects.filter(product=single_product, cart__cart_id=carts.views._cart_id(self.request)).exists()
+            in_cart = CartItem.objects.filter(product=single_product,
+                                              cart__cart_id=carts.views._cart_id(self.request)).exists()
         except Exception as e:
             raise e
 
@@ -49,17 +62,34 @@ class ProductDetailView(TemplateView):
         return context
 
 
+class SearchListView(ListView):
+    template_name = 'store/store.html'
+    model = Product
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(SearchListView, self).get_context_data(**kwargs)
 
+        # paginando produtos pesquisados
 
+        if 'search_key' in self.request.GET and self.request.GET.get('search_key'):
+            search_key = self.request.GET.get('search_key')
+            products_search = Product.objects.all().filter(product_name__icontains=search_key, is_available=True).order_by('id')
 
+            page = self.request.GET.get('page')
+            search_key = self.request.GET.get('search_key')
+            paginator = Paginator(products_search, 5)
 
+            paged_products = paginator.get_page(page)
 
+            context['products'] = paged_products
+            context['search_key'] = search_key
 
+            # numero de produtos encotrados na pesquisa
+            context['quant_products'] = products_search.count()
 
+        else:
+            context['quant_products'] = 0
+            context['search_key'] = ''
+            context['products'] = ''
 
-
-
-
-
-
+        return context
